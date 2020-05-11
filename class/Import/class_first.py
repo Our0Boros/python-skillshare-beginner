297090560b7d30e6e4885214c93e76833c89645f
Our0Boros <Our0Boros@users.noreply.github.com>
2020-05-11 15:20:09 +0300

    Update class_first.py

--- class/Import/class_first.py
+++ class/Import/class_first.py
@@ -27,13 +27,13 @@ class  Book(): #Capitalize names refer to a class in python
 		"""Simulate an ebook reader"""
 		print("\nLibrary: " + self.name.title() + "\nPrice: $" + str(self.price) + "\nPublisher: " + self.publisher + ".\n")
 
-# # Creat an instance of our Book class
-# my_book = Book('elon musk', 14.99, 'virgin books')
-# your_book = Book('the everything store', 9.99, 'virgin books')
+# Creat an instance of our Book class
+my_book = Book('elon musk', 14.99, 'virgin books')
+your_book = Book('the everything store', 9.99, 'virgin books')
 
-# # Accessing attributes
-# print("I'm currently reading " + my_book.name.title() + ".")
-# print("This books cost: $%.2f." % my_book.price)
+# Accessing attributes
+print("I'm currently reading " + my_book.name.title() + ".")
+print("This books cost: $%.2f." % my_book.price)
 
 # # Calling our ebook_reader method
 # my_book.ebook_reader()
