9fcda1d3d22efbed91f49f44e6c7a2b092faa480
Our0Boros <Our0Boros@users.noreply.github.com>
2020-05-11 14:59:53 +0300

    Create import class.py

---
+++ class/Import/import class.py
@@ -0,0 +1,9 @@
+### 11/05/2020
+### Author: Omer Goder
+### Importing a class from a module
+
+from class_first import Book
+
+my_book = Book('elon musk', 14.99, 'virgin books')
+
+my_book.ebook_reader()
