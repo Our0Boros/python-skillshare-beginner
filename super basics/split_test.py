41732e25e04e95f25061b731b8cc60a51d8306dc
Our0Boros <Our0Boros@users.noreply.github.com>
2020-05-15 13:00:22 +0300

    Create split_test.py

---
+++ super basics/split_test.py
@@ -0,0 +1,9 @@
+### 15/05/2020
+### Author: Omer Goder
+### Testing the split() function
+
+str = 'hello world and welcome to the show'
+print(str.split(' ', 4))
+
+for word in range(len(str.split())):
+	print(str.split()[word])
